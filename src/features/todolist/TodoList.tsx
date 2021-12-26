7e97c2685189ac56a26efb21d9b1f5503f5ba12b
Shakib Farzan <farzan.shakib@gmail.com>
2021-12-26 23:46:03 +0330

    done static table

--- src/features/todolist/TodoList.tsx
+++ src/features/todolist/TodoList.tsx
@@ -16,7 +16,7 @@ const TodoList = (): React.ReactElement => {
       </thead>
       <tbody>
         <tr className="border-b">
-          <td className="text-center py-6">
+          <td className="text-center py-6 px-1">
             <input
               className="rounded border-gray-300 border-2 focus:ring-0"
               type="checkbox"
@@ -24,18 +24,20 @@ const TodoList = (): React.ReactElement => {
               id="checkbox"
             />
           </td>
-          <td className="text-center font-semibold">Task 1</td>
-          <td className="text-center">
+          <td className="text-center font-semibold py-6 px-1">Task 1</td>
+          <td className="text-center py-6 px-1">
             <Badge content="Paused" mode="warning" />
           </td>
-          <td className="text-center font-semibold">21 October 2021</td>
-          <td className="text-center font-semibold">09:30 AM</td>
-          <td className="text-center">
+          <td className="text-center font-semibold py-6 px-1">
+            21 October 2021
+          </td>
+          <td className="text-center font-semibold py-6 px-1">09:30 AM</td>
+          <td className="text-center py-6 px-1">
             <PairButton />
           </td>
         </tr>
         <tr className="border-b">
-          <td className="text-center py-6">
+          <td className="text-center py-6 px-1">
             <input
               className="rounded border-gray-300 border-2 focus:ring-0"
               type="checkbox"
@@ -43,13 +45,15 @@ const TodoList = (): React.ReactElement => {
               id="checkbox"
             />
           </td>
-          <td className="text-center font-semibold">Task 1</td>
-          <td className="text-center">
+          <td className="text-center py-6 px-1 font-semibold">Task 1</td>
+          <td className="text-center py-6 px-1">
             <Badge content={'In progress'} mode={'primary'} />
           </td>
-          <td className="text-center font-semibold">21 October 2021</td>
-          <td className="text-center font-semibold">09:30 AM</td>
-          <td className="text-center">
+          <td className="text-center py-6 px-1 font-semibold">
+            21 October 2021
+          </td>
+          <td className="text-center py-6 px-1 font-semibold">09:30 AM</td>
+          <td className="text-center py-6 px-1">
             <PairButton />
           </td>
         </tr>
