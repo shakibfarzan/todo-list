00367fabaf77be723b0f4d126e46b9118fbdd2ae
Shakib Farzan <farzan.shakib@gmail.com>
2021-12-27 17:22:06 +0330

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import './App.css';
 import TodoList from './features/todolist/TodoList';
 import MyTabs from './features/components/MyTabs';
+import AddButton from './features/components/AddButton';
 
 function App(): React.ReactElement {
   return (
-    <div className="flex flex-col mx-4 my-10">
-      <MyTabs
-        items={[
-          { id: 1, title: 'To Do', content: <TodoList /> },
-          { id: 2, title: 'Done Tasks', content: <TodoList /> },
-        ]}
-      />
-    </div>
+    <>
+      <div className="flex flex-col mx-4 my-10">
+        <AddButton />
+        <MyTabs
+          items={[
+            { id: 1, title: 'To Do', content: <TodoList /> },
+            { id: 2, title: 'Done Tasks', content: <TodoList /> },
+          ]}
+        />
+      </div>
+    </>
   );
 }
 
