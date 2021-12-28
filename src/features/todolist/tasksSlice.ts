16aa1dcef034cf4b73c4811b1472c623c3fe17cd
Shakib Farzan <farzan.shakib@gmail.com>
2021-12-28 10:02:02 +0330

    add task edit and task remove actions

--- src/features/todolist/tasksSlice.ts
+++ src/features/todolist/tasksSlice.ts
@@ -3,6 +3,7 @@ import {
   PayloadAction,
   createSelector,
   Slice,
+  Selector,
 } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
@@ -24,6 +25,20 @@ const taskSlice: Slice = createSlice({
     taskAdded: (state, action: PayloadAction<Omit<TaskType, 'id'>>) => {
       state.push({ id: lastId++, ...action.payload });
     },
+    taskEdited: (state, action: PayloadAction<TaskType>) => {
+      const index = state.findIndex(
+        (task: TaskType) => task.id === action.payload.id,
+      );
+      state[index].title = action.payload.title;
+      state[index].status = action.payload.status;
+      state[index].date = action.payload.date;
+    },
+    taskRemoved: (state, action: PayloadAction<Pick<TaskType, 'id'>>) => {
+      const index = state.findIndex(
+        (task: TaskType) => task.id === action.payload.id,
+      );
+      state.splice(index, 1);
+    },
   },
 });
 
@@ -36,6 +51,12 @@ export const getDoneTasks = createSelector(
   (state: RootState) => state.tasks,
   (tasks) => tasks.filter((task: TaskType) => task.status === 'Done'),
 );
+
+export const getTaskById = (taskId: number): Selector<RootState, TaskType> =>
+  createSelector(
+    (state: RootState) => state.tasks,
+    (tasks) => tasks.find((task: TaskType) => task.id === taskId),
+  );
 
 export const { taskAdded } = taskSlice.actions;
 
